955421848c64a492b584a6a6cb89e757cf439e1b
Maxime Michaud <14856114+MaximeMichaud@users.noreply.github.com>
2020-10-11 14:17:09 -0400

    zlib added
    note : maybe should use an up-to-date version

--- kvs-install.sh
+++ kvs-install.sh
@@ -198,7 +198,7 @@ function aptupdate() {
   apt-get update
 }
 function aptinstall() {
-  apt-get -y install ca-certificates apt-transport-https dirmngr zip unzip lsb-release gnupg openssl curl wget memcached
+  apt-get -y install ca-certificates apt-transport-https dirmngr zip unzip lsb-release gnupg openssl curl wget memcached zlib1g-dev
 }
 
 function aptinstall_nginx() {
