1e944e24ee6f18e32793e36dc6cf6a3b1a9e45f8
Maxime Michaud <14856114+MaximeMichaud@users.noreply.github.com>
2020-12-14 16:22:23 -0500

    ShellCheck SC2006

--- conf/nginx/update-cloudflare-ip-list.sh
+++ conf/nginx/update-cloudflare-ip-list.sh
@@ -6,11 +6,11 @@
 echo -n > /etc/nginx/globals/cloudflare-ip-list.conf;
 
 # fetch and update ipv4
-for i in `curl -s https://www.cloudflare.com/ips-v4`; do
+for i in $(curl -s https://www.cloudflare.com/ips-v4); do
     echo "set_real_ip_from $i;" >> /etc/nginx/globals/cloudflare-ip-list.conf;
 done
 
 # fetch and update ipv6
-for i in `curl -s https://www.cloudflare.com/ips-v6`; do
+for i in $(curl -s https://www.cloudflare.com/ips-v6); do
     echo "set_real_ip_from $i;" >> /etc/nginx/globals/cloudflare-ip-list.conf;
 done
