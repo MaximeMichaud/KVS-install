0492652d2af114f1f40287850b7ce9e5fc5d9a6c
Maxime Michaud <14856114+MaximeMichaud@users.noreply.github.com>
2020-12-14 15:53:02 -0500

    PHP 7.3 Security fixes only

--- kvs-install.sh
+++ kvs-install.sh
@@ -112,7 +112,7 @@ function installQuestions() {
   echo ""
   echo "${cyan}Which Version of PHP ?"
   echo "${red}Red = End of life ${yellow}| Yellow = Security fixes only ${green}| Green = Active support"
-  echo "${green}   1) PHP 7.3 "
+  echo "${yellow}   1) PHP 7.3 "
   echo "   2) PHP 7.4 (recommended) ${normal}${cyan}"
   until [[ "$PHP_VERSION" =~ ^[1-2]$ ]]; do
     read -rp "Version [1-2]: " -e -i 2 PHP_VERSION
